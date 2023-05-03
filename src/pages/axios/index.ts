adf7b32f883a408b1744ae98b56fcd452b6654bf
jeonyul <jeonyul00@gmail.com>
2023-05-03 13:04:49 +0900

    react type

--- src/pages/axios/index.ts
+++ src/pages/axios/index.ts
@@ -1,9 +1,23 @@
-import axios, { Axios, AxiosError, AxiosResponse, isAxiosError } from "axios";
+import axios, {
+  Axios,
+  AxiosError,
+  AxiosResponse,
+  AxiosResponseTransformer,
+  isAxiosError,
+} from "axios";
 
 interface Post {
   userId: number;
   number: number;
   title: string;
+}
+
+interface yulInter {
+  get: <T, R = AxiosResponse<T>>(url: string) => Promise<R>;
+  post: <T, R = AxiosResponse<T>>(url: string, data: any) => Promise<R>;
+  (config: {}): void;
+  (url: string, config: {}): void;
+  isAxiosError: <T>(error: T) => error is T;
 }
 
 (async () => {
@@ -32,6 +46,17 @@ interface Post {
       console.log(
         (error.response as AxiosResponse<{ message: string }>)?.data.message
       );
+    }
+  }
+  // --
+  try {
+    const yulAxios: yulInter = axios;
+    const value = await yulAxios.get(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+  } catch (e) {
+    if (isAxiosError(e)) {
+      console.log(e);
     }
   }
 })();
--- src/pages/react/hook.tsx
+++ src/pages/react/hook.tsx
@@ -1,27 +1,79 @@
-import React, {
-  FC,
-  FunctionComponent,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+// import React, {
+//   FC,
+//   FunctionComponent,
+//   useCallback,
+//   useEffect,
+//   useRef,
+//   useState,
+// } from "react";
 
-interface P {
-  name: string;
-  age: number;
-}
+// interface P {
+//   name: string;
+//   age: number;
+// }
 
-// FunctionComponent === FC
-const hook: FC<P> = (props) => {
-  const [ex, setEx] = useState();
-  const focus = useRef();
+// // FunctionComponent === FC
+// const hook: FC<P> = (props) => {
+//   const [ex, setEx] = useState();
+//   const focus = useRef();
 
-  useEffect(() => {}, []);
+//   useEffect(() => {}, []);
 
-  const exFunc = useCallback(() => {}, []);
+//   const exFunc = useCallback(() => {}, []);
 
-  return <div>hook</div>;
+//   return <div>hook</div>;
+// };
+
+// export default hook;
+import * as React from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
+
+const WordRelay: React.FC = () => {
+  const [word, setWord] = useState("yul");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, []);
+
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      const input = inputEl.current;
+      if (word[word.length - 1] === value[0]) {
+        setResult("딩동댕");
+        setWord(value);
+        setValue("");
+        if (input) {
+          input.focus();
+        }
+      } else {
+        setResult("땡");
+        setValue("");
+        if (input) {
+          input.focus();
+        }
+      }
+    },
+    [word, value]
+  );
+
+  const onChange = useCallback((e) => {
+    setValue(e.currentTarget.value);
+  }, []);
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputEl} value={value} onChange={onChange} />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
 };
 
-export default hook;
+export default WordRelay;
