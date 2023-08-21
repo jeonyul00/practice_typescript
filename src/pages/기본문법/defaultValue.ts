dae1f0bb960741627f9a9e89fcc8681a342d9d3f
jeonyul <jeonyul00@gmail.com>
2023-08-21 14:13:58 +0900

    reset : project

--- src/pages/기본문법/defaultValue.ts
+++
@@ -1,8 +0,0 @@
-// 기본값
-const defaultValue = (a: number = 1, b: string = "string"): void => {};
-const defaultValueCallback = (
-  callback: { children: string } = { children: "jeonyul" }
-): string => {
-  return callback.children;
-};
-const aaaa = <T = unknown>(x: T, y: T): void => {};
